c10c8daebaf9275c219f83eeff16ac5323619484
ManosTs <mtsiorbas@gmail.com>
2020-06-02 11:40:44 +0300

    z

---
+++ src/Owner.java
@@ -0,0 +1,19 @@
+public class Owner extends User {
+    private boolean isAdmin = true;
+
+    private int pin;
+
+    public Owner(){
+    }
+
+    public void setPin(int p){
+        this.pin = p;
+    }
+
+    public int getPin(){
+        return pin;
+    }
+
+
+
+}
